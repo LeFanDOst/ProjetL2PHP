52f49d9ac33b9b1e57d3e53c228068db24c6487f
LeFanDOst <45673601+LeFanDOst@users.noreply.github.com>
2021-05-30 17:03:46 +0200

    Update APropos.php

--- php/APropos.php
+++ php/APropos.php
@@ -14,9 +14,7 @@
 				color: white;
 				text-align: justify;
 			}
-
 			a { color:  #fdfefe ; }
-
 			a:hover {
 				color:#0073e6;
 			 }
@@ -31,54 +29,50 @@
 		</div>
 		<div class="cadre">
 			<h1>
-				<p>fonctionnement</p>
+				<p>Fonctionnement</p>
 			</h1>
 			<hr>
-			<h2>bienvenu sur notre sites de gestion de tournois sportif voici quelque information utile / importante pour comprendre comment correctement utilisé ce site.</h2>
-			<p>dans un premier temps nous vous invitons a vous <a href="./Register.php" >créer un compte</a> adapté a ce que vous voulez faire :</p>
+			<h2>Bienvenue sur notre site de gestion de tournois sportif. Voici quelque informations utiles / importantes pour comprendre comment correctement utiliser ce site.</h2>
+			<p>Dans un premier temps, nous vous invitons à vous <a href="./Inscription.php" >créer un compte</a> adapté à ce que vous voulez faire :</p>
 			<ul>
-					<li> utilisateur si vous voulez voir l'avancement des tournois</li>
-					<li>joueur si vous voulez rentrer dans une équipe et prendre par au tournois</li>
-					<li>capitaine si vous voulez créer une équipe et choisir dans quel tournois jouer</li>
+					<li><b>utilisateur</b>, si vous voulez voir l'avancement des tournois</li>
+					<li><b>joueur</b>, si vous voulez rentrer dans une équipe et prendre part aux tournois</li>
+					<li><b>capitaine</b>, si vous voulez créer une équipe et choisir dans quel.s tournoi.s jouer</li>
 			</ul>
-			<p>si vous voulez créer un tournois  il faudra contacter les administrateurs , les information nécessaire pour ce faire sont dans <a href="./contact.php" >contact</a> . </p>
-			<p> vous devrez communiquer : </p>
+			<p>Si vous voulez créer un tournoi il faudra contacter les administrateurs, les information nécessaire pour ce faire sont dans <a href="./contact.php" >contact</a>. </p>
+			<p>Vous devrez communiquer :</p>
 			<ul>
-					<li> le nom de votre compte</li>
-					<li>le nom du tournois </li>
-					<li>le type de tournois <a style="color: red;"> (entre coupe,tournois,championnat ) </a></li>
-					<li>le lieu <a style="color: red;"> (en france) </a></li>
-					<li>la date de début </li>
+					<li>le nom de votre compte</li>
+					<li>le nom du tournoi</li>
+					<li>le type de tournoi <a style="color: red;">(entre coupe, tournois, championnat )</a></li>
+					<li>le lieu <a style="color: red;">(en France)</a></li>
+					<li>la date de début</li>
 					<li>la durée</li>
-					<li>le nombre d'équipes <a style="color: red;"> (une puissance de 2) </a></li>
+					<li>le nombre d'équipes <a style="color: red;">(une puissance de 2)</a></li>
 			</ul>
-			<p> votre tournois sera alors créé dans les plus bref délai et vous en serais le gestionnaire; ce sera donc a vous de validé les inscription , daté les matches et d'inscrire les scores </p>
-			<h2>inscription au tournois :</h2>
-			<p> seul le capitaine d'une équipe peux pré-inscrire cette dernière , le gestionnaire du tournois validera ou non la pré-inscription </p>
+			<p> Votre tournoi sera alors créé dans les plus bref délais et vous en serez le gestionnaire ; ce sera donc à vous de valider les inscriptions, dater les matchs et d'inscrire les scores.</p>
+			<h2>Inscriptions aux tournois :</h2>
+			<p>Seul le capitaine d'une équipe peut pré-inscrire cette dernière, le gestionnaire du tournoi validera ou non la pré-inscription.</p>
 			<h1>
 				<p>Les types de tournois</p>
 			</h1>
 			<hr>
-			<h2>championnat  </h2>
-			 	<p> toutes les équipes s'affronte des point sont attribué selon les victoires / nuls/ défaite , l'équipe qui a le plus de point quand tout les matches sont joué gagne</p> 
+			<h2>Championnat</h2>
+			 	<p>Toutes les équipes s'affrontent. Des points sont attribués selon les victoires / matchs nuls / défaites. L'équipe qui a le plus de point quand tout les matchs sont joués gagne.</p> 
 				<p>L’attribution des points s’organise de la manière suivante :</p>
 				<ul>
 					<li>victoire = 4 points</li>
 					<li>match nul = 2 points</li>
 					<li>défaite = 1 point</li>
 				</ul>
-			<h2>coupe </h2>
-			<p>chaque tour les équipe s'affronte 2 a 2 la perdante est éliminé et la gagnante passe au tour suivant</p>
-
-			<h2>tournois </h2>
-			<p> séparé en deux partie : </p>
+			<h2>Coupe</h2>
+			<p>Chaque tour, les équipe s'affrontent 2 à 2. La perdante est éliminée et la gagnante passe au tour suivant.</p>
+			<h2>Tournois</h2>
+			<p>Séparé en deux parties :</p>
 			<ul>
-					<li>une phase de poule ou les équipe sont répartie en groupe de 4 et font un championnat entre elle puis les deux premiére passe a la phase final</li>
-					<li>une phase final ou les équipes s'affronte comme lors d'une coupe (les premiéreq équipes de chaque poule affrontent les deuxiémes d'une autre poule) </li>
-
+				<li>une phase de poule, où les équipe sont réparties en groupe de 4 et font un championnat entre elle, puis les deux première passent à la phase finale</li>
+				<li>une phase finale, où les équipes s'affrontent comme lors d'une coupe (les premières équipes de chaque poule affrontent les deuxièmes d'une autre poule)</li>
 			</ul>
-
-
 		</div>
 	</body>
 </html>
