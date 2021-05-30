09a9bd3d0f081a284d9b06044bf3f5dfd6a35b34
ozgur142 <53865585+ozgur142@users.noreply.github.com>
2021-05-30 13:23:28 +0200

    Add files via upload

---
+++ php/APropos.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<link rel="stylesheet" type="text/css" href=".././css/styleTournois.css" />
+		<title> Liste des Tournois </title>
+		<style>
+			body .bandeau-haut img {
+				width:70px;
+				padding:5px 0 0 5px;
+				margin:5px 0 0 5px;
+				float:left;
+			}
+			p, h2, li, ul {
+				color: white;
+				text-align: justify;
+			}
+		</style>
+	</head>
+
+	<body>
+		<div class="bandeau-haut">
+			<a href="../index.php">
+				<img src="../img/prev.png">
+				<h3>RETOUR</h3>
+			</a>
+		</div>
+
+		<div class="cadre">   
+			<h1>
+				<p>Les Règlements</p>
+			</h1>
+
+			<hr>
+
+			<h2 style="color: red;">ARTICLE 1 : ATTRIBUTION DES POINTS</h2>
+				<p>L’attribution des points s’organise de la manière suivante :</p>
+				<ul>
+					<li>victoire = 4 points</li>
+					<li>match nul = 2 points</li>
+					<li>défaite = 1 point</li>
+				</ul>
+
+
+
+		</div>
+
+
+	</body>
+</html>
