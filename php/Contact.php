07a16454b700abf9367e18009995c14764beb579
Onur Tan Kulan <46331545+Tnkln99@users.noreply.github.com>
2021-05-31 21:57:35 +0200

    Add files via upload

---
+++ php/Contact.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" style="text/css" href=".././css/styleTournois.css"/>
+		<title>Contact</title>
+		<style>
+			body .bandeau-haut img {
+				width:70px;
+				padding:5px 0 0 5px;
+				margin:5px 0 0 5px;
+				float:left;
+			}
+			p, h2, li, ul {
+				color: white;
+				text-align: justify;
+			}
+			a { color:  #fdfefe ; }
+			a:hover {
+				color:#0073e6;
+			 }
+		</style>
+	</head>
+	
+	<body>
+		<div class="bandeau-haut">
+			<a href="../index.php">
+				<img src="../img/prev.png">
+				<h3>RETOUR</h3>
+			</a>
+		</div>
+			
+		<div class="cadre">
+			<h1 style="color: white;">Contact </h1>
+			
+			<p>Voici les développeurs de ce site, avec les données de contact de chacun d'entre eux.</p>
+			
+			<ul class="listeContact">
+				<li>BEHINAN Ophélia</li>
+				<li>DOGAN Ozgur</li>
+				<li>KULAN Onur Tan</li>
+				<li>PUJADE Joffrey</li>
+				<li>TISSERANT Tom</li>
+			</ul>
+		</div>
+	</body>
+</html>
